682f3618e988bece38e97e24b3926b4ec00fa5d0
Eamon Sisk <esisk@protonmail.com>
2025-10-15 23:44:00 +0200

    Include css in dist

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  copy: ["src/cantal.css", { from: "src/cantal.css", to: "dist/cantal.css" }],
   entry: ["./src/index.ts"],
   minify: true,
 });
